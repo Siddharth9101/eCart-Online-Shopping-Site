f1d8b5a358c699a2b9402e2453e8fe3ba6352870
Siddharth Saxena <siddharthsaxena9101@gmail.com>
2024-10-31 11:40:28 +0530

    Remove evn

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,11 @@
-require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 
 const app = express();
 try {
-  mongoose.connect(process.env.DB_URL);
+  mongoose.connect(
+    "mongodb+srv://siddharthsaxena9101:Id7N6zwFFZPlMIQE@cluster0.pr0e4.mongodb.net/ecom"
+  );
   console.log("Connected to the database");
 } catch (error) {
   console.error("Error while connecting to the database");
